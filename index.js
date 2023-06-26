ea5a528f92654509b812256edff45d72f8ffb716
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-06-26 11:34:21 -0300

    operation menu options

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ const inquirer = require('inquirer');
 
 // modulos internos
 const fs = require('fs');
+
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+          'Criar conta',
+          'Consultar saldo',
+          'Depositar',
+          'Sacar',
+          'Sair',
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer['action'];
+      console.log(action);
+    })
+    .catch((err) => console.log(err));
+}
