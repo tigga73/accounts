35a742ab9959ffc3172bd9cf593d2d610a95a44b
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-06-26 11:54:34 -0300

    adicionando criação de conta

--- index.js
+++ index.js
@@ -25,7 +25,61 @@ function operation() {
     ])
     .then((answer) => {
       const action = answer['action'];
-      console.log(action);
+
+      switch (action) {
+        case 'Criar conta':
+          createAccount();
+        default:
+          break;
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+function createAccount() {
+  console.log(chalk.bgGreen.black('Obrigado por escolher o nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para sua conta:',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['accountName'];
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts');
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('Esta conta já existe. Escolha outro nome'),
+        );
+
+        buildAccount();
+        return;
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{ "balance": 0 }',
+        function (err) {
+          console.log(err);
+        },
+      );
+
+      console.log(
+        chalk.bgGreen.black('Parabéns! Sua conta foi criada com sucesso!'),
+      );
+
+      operation();
     })
     .catch((err) => console.log(err));
 }
